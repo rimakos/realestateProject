e8af763989fa00dc2832c9a85f57f7133e8a0cb6
Megi Kuka <megi.kuka@imb.al>
2021-05-16 17:04:27 +0200

    Blog html

--- src/main/java/com/example/demo/controller/thymeleafController/PostControllerThyme.java
+++ src/main/java/com/example/demo/controller/thymeleafController/PostControllerThyme.java
@@ -1,2 +1,22 @@
-package com.example.demo.controller.thymeleafController;public class PostControllerThyme {
+package com.example.demo.controller.thymeleafController;
+
+import com.example.demo.service.post.PostService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PostControllerThyme {
+    private PostService postService;
+
+    public PostControllerThyme(final PostService postService) {
+        this.postService=postService;
+    }
+
+    @GetMapping("/blog")
+    public String posts(final ModelMap modelMap) {
+        var blogList = this.postService.findAll();
+        modelMap.addAttribute("blogs", blogList);
+        return "blog";
+    }
 }
