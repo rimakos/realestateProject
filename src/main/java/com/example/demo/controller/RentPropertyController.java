dcc8b1a9516489639c68857fa184129fbfb0e36a
Perparim Koci <rimakosrimi93@gmail.com>
2021-05-16 11:38:58 +0200

    buy/rent controllers

---
+++ src/main/java/com/example/demo/controller/RentPropertyController.java
@@ -0,0 +1,23 @@
+package com.example.demo.controller;
+
+import com.example.demo.enumerations.PropertyStatus;
+import com.example.demo.service.property.PropertyService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RentPropertyController {
+    private PropertyService propertyService;
+
+    public RentPropertyController(final PropertyService propertyService) {
+        this.propertyService = propertyService;
+
+    }
+    @GetMapping("/rent")
+    public String buy(final ModelMap modelMap) {
+        var rentStatus = this.propertyService.findPropertiesByStatus(PropertyStatus.Rent);
+        modelMap.addAttribute("rentStatus", rentStatus);
+        return "rent";
+    }
+}
